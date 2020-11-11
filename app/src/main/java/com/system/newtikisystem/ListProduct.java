c6e59e62172eebe4af91ef7ffb4eb5697bd8f63a
anhthao-72 <anhthao721999@gmail.com>
2020-11-11 09:58:41 +0700

    Thao with luv

--- app/src/main/java/com/system/newtikisystem/ListProduct.java
+++ app/src/main/java/com/system/newtikisystem/ListProduct.java
@@ -23,12 +23,12 @@ public class ListProduct extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.lpRecyclerView);
         ArrayList<Products> products = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            products.add(new Products("Thao", 450, "https://tatyanaseverydayfood.com/wp-content/uploads/2018/07/Summer-Sangria-Cake-4.jpg"));
-            RecyclerAdapterListProduct adapter = new RecyclerAdapterListProduct(products);
-            recyclerView.setAdapter(adapter);
-            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-            recyclerView.setLayoutManager(layoutManager);
-        }
+//        for (int i = 1; i < 10; i++) {
+//            products.add(new Products("Thao", 450, "https://tatyanaseverydayfood.com/wp-content/uploads/2018/07/Summer-Sangria-Cake-4.jpg"));
+//            RecyclerAdapterListProduct adapter = new RecyclerAdapterListProduct(products);
+//            recyclerView.setAdapter(adapter);
+//            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//            recyclerView.setLayoutManager(layoutManager);
+//        }
     }
 }
