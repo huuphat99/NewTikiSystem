b22e5726c7d06696ff062f45530f46dcffbec568
= <=>
2020-11-16 21:42:12 +0700

    update

--- app/src/main/java/com/system/newtikisystem/controller/MainActivity.java
+++ app/src/main/java/com/system/newtikisystem/controller/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         txtUsername = findViewById(R.id.txtEmail);
         txtPassword = findViewById(R.id.txtPassword);
         alert = findViewById(R.id.alert);
